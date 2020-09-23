45bf3c1e97380fb956b52e4b28928c89fa57447e
zapto123 <tsoorckanukris@gmail.com>
2020-09-23 19:13:37 +0300

    Implemented matrix parsing algorithm

---
+++ Exercitiul_1_a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Core;
+
+namespace Exercitiul_1_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Graph graph = new Graph();
+            
+            graph.SetIncidenceMatrix(graph.ParseMatrixFile());
+            graph.PrintAdjacencyMatrix();
+        }
+    }
+}
